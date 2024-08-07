1f5ffc53bf88176c9b4d3a544cbcdd9f18d1317d
Marius Vatasoiu <marius.vatasoiu@outlook.com>
2024-08-08 00:22:16 +0300

    fix: safety

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,13 @@
-console.log("main.js");
-
 function init() {
   const drawerEl = document.querySelector(".drawer");
   const openDrawerBtn = document.querySelector(".drawer-open");
   const closeDrawerBtn = document.querySelector(".drawer-close");
 
-  openDrawerBtn.addEventListener("click", () => {
+  openDrawerBtn?.addEventListener?.("click", () => {
     drawerEl.classList.remove("hidden");
   });
 
-  closeDrawerBtn.addEventListener("click", () => {
+  closeDrawerBtn?.addEventListener?.("click", () => {
     drawerEl.classList.add("hidden");
   });
 }
